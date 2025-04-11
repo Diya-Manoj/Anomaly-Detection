3e96a45b802798f9eaecf7f04fcba1bbf92cc4c1
Diya Manoj <diya.sumigokul@gmail.com>
2025-04-11 19:45:13 +0530

    Update model.py

--- model.py
+++ model.py
@@ -4,28 +4,19 @@ import torch.nn as nn
 class ConvAutoencoder(nn.Module):
     def __init__(self):
         super(ConvAutoencoder, self).__init__()
-
-        # Encoder
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 32, 3, stride=2, padding=1),  # -> (32, 64, 64)
+            nn.Conv2d(1, 16, 3, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(32, 64, 3, stride=2, padding=1), # -> (64, 32, 32)
+            nn.Conv2d(16, 32, 3, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(64, 128, 3, stride=2, padding=1), # -> (128, 16, 16)
+            nn.Conv2d(32, 64, 3, stride=2, padding=1),
             nn.ReLU()
         )
-
-        # Decoder
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1), # -> (64, 32, 32)
+            nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1),  # -> (32, 64, 64)
+            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(32, 1, 3, stride=2, padding=1, output_padding=1),   # -> (1, 128, 128)
+            nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1),
             nn.Sigmoid()
         )
-
-    def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
